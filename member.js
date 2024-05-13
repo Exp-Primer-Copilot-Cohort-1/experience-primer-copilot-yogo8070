aa1e7c06e7209a64110251abecf2f04fd2ae4e39
yogo8070 <167827574+yogo8070@users.noreply.github.com>
2024-05-13 21:36:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    const skills = {
+        name: 'John',
+        age: 30,
+        skills: ['JS', 'React', 'Node'],
+        isMarried: false,
+    };
+    return skills;
+}
